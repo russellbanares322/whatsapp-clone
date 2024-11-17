3e2fc38b91eb719a7b4e955709a4963aebf21561
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-17 20:49:42 +0800

    chore: change page sample text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen w-full font-[family-name:var(--font-geist-sans)]">
-      <h1 className="flex items-center justify-center min-h-screen">
-        WhatsApp Clone
-      </h1>
+      <h1 className="flex items-center justify-center min-h-screen">Content</h1>
     </div>
   );
 }
