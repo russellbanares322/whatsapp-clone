ede9cf0e7590e897dded2aa2ee64b2469ff7c928
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-19 21:08:41 +0800

    feat: put whatsapp icon in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { FaWhatsapp } from "react-icons/fa";
+
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
       <div className="flex flex-col items-center gap-2 justify-center min-h-screen">
+        <FaWhatsapp className="text-gray-300" size={75} />
         <p>WhatsApp Clone for Browser</p>
         <p className="font-light text-[.9rem]">Send and receive message.</p>
       </div>
