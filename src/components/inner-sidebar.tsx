66e170fb02a96155cc00b1a7173189b5da489b8e
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-19 21:46:40 +0800

    feat: add icons in inner sidebar's header

--- src/components/inner-sidebar.tsx
+++ src/components/inner-sidebar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { IoCreateOutline } from "react-icons/io5";
+import { BsFilter } from "react-icons/bs";
 
 const InnerSidebar = () => {
   return (
@@ -6,10 +7,10 @@ const InnerSidebar = () => {
       <div className="max-h-[887px] overflow-auto px-[21px] py-[27px]">
         {/* Header */}
         <div className="flex justify-between items-center mb-[24px]">
-          <p className="font-semibold">Chats</p>
-          <div className="flex items-center gap-2">
-            <p>N</p>
-            <p>F</p>
+          <p className="font-semibold text-xl">Chats</p>
+          <div className="flex items-center gap-4">
+            <IoCreateOutline size={20} />
+            <BsFilter className="pt-1" size={26} />
           </div>
         </div>
       </div>
