40b3f18cd16277e9091f8e68909dd74833c13c74
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-20 20:58:28 +0800

    feat: add placeholder in search input

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -5,7 +5,11 @@ const SearchInput = () => {
   return (
     <div className="inline-flex px-2 h-[32px] rounded-md gap-2 items-center border border-solid border-gray-300">
       <LiaSearchSolid className="text-gray-500" size={14} />
-      <input className="outline-none border-none text-sm" type="text" />
+      <input
+        className="outline-none border-none text-sm"
+        type="text"
+        placeholder="Search or start a new chat"
+      />
     </div>
   );
 };
