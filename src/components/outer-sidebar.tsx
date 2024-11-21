3dec0ab7a2cec2e936bbe2436bc2c1e835c455c0
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-21 21:34:12 +0800

    feat: import menu icon in outer-sidebar component

--- src/components/outer-sidebar.tsx
+++ src/components/outer-sidebar.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import { IoMenuOutline } from "react-icons/io5";
 
 const OuterSidebar = () => {
   // Implement sidebar functionality
-  return <div className="w-[40px]"></div>;
+  return (
+    <div className="w-[40px]">
+      <div>
+        <IoMenuOutline />
+      </div>
+    </div>
+  );
 };
 
 export default OuterSidebar;
