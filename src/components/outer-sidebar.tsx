25174fab03e27ffb69ab7facb9380415dd449ff9
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-22 20:14:00 +0800

    feat: display outer sidebar icons

--- src/components/outer-sidebar.tsx
+++ src/components/outer-sidebar.tsx
@@ -1,11 +1,22 @@
 import { IoMenuOutline } from "react-icons/io5";
+import { BsChatText } from "react-icons/bs";
+import { FiPhone } from "react-icons/fi";
 
 const OuterSidebar = () => {
   // Implement sidebar functionality
   return (
     <div className="w-[40px]">
-      <div>
-        <IoMenuOutline />
+      <div className="mt-[50px] ml-2">
+        <IoMenuOutline
+          className="hover:bg-[#ededed] p-2 rounded-md"
+          size={35}
+        />
+        <div className="mt-6 space-y-3">
+          <BsChatText className="hover:bg-[#ededed] p-2 rounded-md" size={33} />
+          <div className="hover:bg-[#ededed] rounded-md ">
+            <FiPhone className="rotate-12 p-2" size={33} />
+          </div>
+        </div>
       </div>
     </div>
   );
