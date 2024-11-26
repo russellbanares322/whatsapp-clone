dbcd742042cbb8a108ccb74ad7a05df9587c4893
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-26 20:29:05 +0800

    feat: display other icons in outer sidebar component

--- src/components/outer-sidebar.tsx
+++ src/components/outer-sidebar.tsx
@@ -5,6 +5,9 @@ import { BsChatText } from "react-icons/bs";
 import { FiPhone } from "react-icons/fi";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { MdOutlineStarBorder } from "react-icons/md";
+import { HiOutlineArchiveBox } from "react-icons/hi2";
+import { LiaCogSolid } from "react-icons/lia";
 
 enum SelectedTab {
   Chats = "Chats",
@@ -26,34 +29,52 @@ const OuterSidebar = () => {
     return selectedTab === tab;
   };
 
+  const buttonIcon = (tab: SelectedTab, icon: React.ReactNode) => {
+    return (
+      <button
+        onClick={() => onSelectTab(tab)}
+        className={twMerge(
+          "hover:bg-active-item rounded-md border-none outline-none cursor-default",
+          isTabSelected(tab) &&
+            "bg-active-item relative before:absolute before:left-0 before:top-0 before:bottom-0 before:my-auto before:h-[16px] before:w-[3px] before:rounded-lg before:bg-green before:content-[''] before:motion-preset-shrink"
+        )}
+      >
+        {icon}
+      </button>
+    );
+  };
+
   return (
-    <div className="w-[35px]">
+    <div className="w-[35px] relative">
       <div className="mt-[50px] ml-[5px]">
         <IoMenuOutline
           className="hover:bg-active-item p-3 rounded-md"
           size={42}
         />
         <div className="mt-6">
-          <button
-            onClick={() => onSelectTab(SelectedTab.Chats)}
-            className={twMerge(
-              "hover:bg-active-item rounded-md border-none outline-none cursor-default",
-              isTabSelected(SelectedTab.Chats) &&
-                "bg-active-item relative before:absolute before:left-0 before:top-0 before:bottom-0 before:my-auto before:h-[16px] before:w-[3px] before:rounded-lg before:bg-green before:content-[''] before:motion-preset-shrink"
-            )}
-          >
+          {buttonIcon(
+            SelectedTab.Chats,
             <BsChatText className="p-3" size={42} />
-          </button>
-          <button
-            onClick={() => onSelectTab(SelectedTab.Calls)}
-            className={twMerge(
-              "hover:bg-active-item rounded-md border-none outline-none cursor-default",
-              isTabSelected(SelectedTab.Calls) &&
-                "bg-active-item relative before:absolute before:left-0 before:top-0 before:bottom-0 before:my-auto before:h-[16px] before:w-[3px] before:rounded-lg before:bg-green before:content-[''] before:motion-preset-shrink"
-            )}
-          >
+          )}
+          {buttonIcon(
+            SelectedTab.Calls,
             <FiPhone className="rotate-12 p-3" size={41} />
-          </button>
+          )}
+        </div>
+        <div className="absolute bottom-16">
+          {buttonIcon(
+            SelectedTab.StarredMessages,
+            <MdOutlineStarBorder className="p-3" size={44} />
+          )}
+          {buttonIcon(
+            SelectedTab.ArchivedChats,
+            <HiOutlineArchiveBox className="p-3" size={42} />
+          )}
+          <hr className="mt-1 mb-2" />
+          <LiaCogSolid
+            className="hover:bg-active-item p-3 rounded-md"
+            size={43}
+          />
         </div>
       </div>
     </div>
