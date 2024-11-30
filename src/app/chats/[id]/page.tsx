4b558d59df2c33a287075df63c07bc44b798780e
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-11-30 10:12:19 +0800

    feat: instantiate layout in chats page

--- src/app/chats/[id]/page.tsx
+++ src/app/chats/[id]/page.tsx
@@ -7,5 +7,14 @@ type PageProps = {
 };
 
 export default function Page({ params }: PageProps) {
-  return <div>{params.id}</div>;
+  return (
+    <div className="w-full flex flex-col">
+      {/* Header */}
+      <header className="bg-white h-[65px]">Header</header>
+      {/* Content */}
+      <div className="bg-whitesmoke h-[888px]">Content</div>
+      {/* Chat Input */}
+      <div className="bg-white h-[65px]">Chat Inputs</div>
+    </div>
+  );
 }
