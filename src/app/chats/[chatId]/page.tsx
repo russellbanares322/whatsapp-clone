83499292621aca1e785dfd667410fa936cb015ef
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-12-01 13:05:04 +0800

    feat: wrap chat id types in params

--- src/app/chats/[chatId]/page.tsx
+++ src/app/chats/[chatId]/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-export default async function Page({ params }: { params: { chatId: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ chatId: string }>;
+}) {
+  const { chatId } = await params;
   return (
     <div className="w-full flex flex-col">
       {/* Header */}
       <header className="bg-white h-[65px]">Header</header>
       {/* Content */}
-      <div className="bg-whitesmoke h-[888px]">{`Product ID ${params.chatId}`}</div>
+      <div className="bg-whitesmoke h-[888px]">{`Product ID ${chatId}`}</div>
       {/* Chat Input */}
       <div className="bg-white h-[65px]">Chat Inputs</div>
     </div>
