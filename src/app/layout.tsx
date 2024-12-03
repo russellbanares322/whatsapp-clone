b2c8d00b161a7616d6e8f76c48c8466f62aa9650
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-12-03 21:53:32 +0800

    feat: instantiate ConvexClientProvider component to use convex actions and retrieve the datas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ContentWrapper from "@/components/content-wrapper";
 import { Roboto } from "next/font/google";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -21,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${roboto.className} antialiased`}>
-        <ContentWrapper>{children}</ContentWrapper>
+        <ConvexClientProvider>
+          <ContentWrapper>{children}</ContentWrapper>
+        </ConvexClientProvider>
       </body>
     </html>
   );
