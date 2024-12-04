648823fc06258b58985611e705615a53321223ba
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-12-04 21:26:26 +0800

    chore: add comment in convex chats functions file

--- convex/chats.ts
+++ convex/chats.ts
@@ -1,8 +1,11 @@
 import { query } from "./_generated/server";
 
+// Queries
 export const get = query({
     args: {},
     handler: async(ctx) => {
         return await ctx.db.query("chats").collect()
     }
 })
+
+// Mutations
