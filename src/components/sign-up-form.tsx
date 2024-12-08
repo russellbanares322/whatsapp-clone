0a53465b10487c4e92c5745a73c8e47ef6716c0e
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-12-08 13:13:26 +0800

    feat: intantiate signup bg

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const SignUpForm = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      SignUpForm
+    <div className="h-full w-full min-h-screen relative">
+      <div className="grid grid-row-2">
+        <div className="bg-green min-h-52">
+          <p className="text-white">WhatsApp</p>
+        </div>
+        <div className="bg-whitesmoke h-screen" />
+      </div>
+      {/* Signup Form */}
+      <div className="absolute mx-auto top-40 bottom-0 right-0 left-0 bg-white max-w-[900px] max-h-96 rounded-sm p-10">
+        SignUpForm
+      </div>
     </div>
   );
 };
