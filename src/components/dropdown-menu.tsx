87a025a1177731601e46a8d4714c18437eb632ed
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-12-16 20:06:03 +0800

    feat: instantiate animation in dropdown menu component

--- src/components/dropdown-menu.tsx
+++ src/components/dropdown-menu.tsx
@@ -1,14 +1,27 @@
+import { Dispatch, SetStateAction } from "react";
+import { twMerge } from "tailwind-merge";
+import { motion } from "motion/react";
+
 type DropdownMenuProps = {
   children: React.ReactNode;
+  content: React.ReactNode;
   open: boolean;
-  onOpenChange: () => void;
+  onOpenChange: Dispatch<SetStateAction<boolean>>;
 };
 
-const DropdownMenu = ({ children }: DropdownMenuProps) => {
+const DropdownMenu = ({ children, content, open }: DropdownMenuProps) => {
   return (
     <div className="relative">
       {children}
-      <div className="absolute bg-white shadow-md">content</div>
+      {open && (
+        <motion.div
+          animate={{ y: 3, animationDelay: "10" }}
+          exit={{ opacity: 0 }}
+          className={twMerge("absolute bg-white shadow-lg p-2 rounded-md")}
+        >
+          {content}
+        </motion.div>
+      )}
     </div>
   );
 };
--- src/components/inner-sidebar.tsx
+++ src/components/inner-sidebar.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { IoCreateOutline } from "react-icons/io5";
 import { BsFilter } from "react-icons/bs";
 import { GoPersonAdd } from "react-icons/go";
 import SearchInput from "./search-input";
 import ChatCard from "./chat-card";
+import DropdownMenu from "./dropdown-menu";
+import { useState } from "react";
 
 const InnerSidebar = () => {
+  const [openAddPersonMenu, setOpenAddPersonMenu] = useState(false);
+
   return (
     <div className="border-r border-0 border-solid w-[268px]">
       <div className="max-h-[887px] overflow-auto px-[10px] py-[27px]">
@@ -12,11 +18,18 @@ const InnerSidebar = () => {
         <div className="flex justify-between items-center mb-4">
           <p className="font-semibold text-xl">Chats</p>
           <div className="flex items-center gap-1">
-            <GoPersonAdd
-              className="hover:bg-whitesmoke p-2 rounded-md"
-              size={34}
-              title="Add person to chat"
-            />
+            <DropdownMenu
+              open={openAddPersonMenu}
+              onOpenChange={setOpenAddPersonMenu}
+              content={<div>Content</div>}
+            >
+              <GoPersonAdd
+                className="hover:bg-whitesmoke p-2 rounded-md"
+                size={34}
+                title="Add person to chat"
+                onClick={() => setOpenAddPersonMenu(!openAddPersonMenu)}
+              />
+            </DropdownMenu>
             <IoCreateOutline
               className="hover:bg-whitesmoke p-2 rounded-md"
               size={34}
