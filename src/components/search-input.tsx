67e1f455fcf4f68199be7bdf48a082c684476ed1
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-01-07 20:38:36 +0800

    feat: add border below search input

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import { LiaSearchSolid } from "react-icons/lia";
 
 const SearchInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <div className="inline-flex px-2 w-full h-[32px] rounded-md gap-2 items-center border border-solid border-gray-300">
+    <div className="inline-flex relative px-2 w-full h-[32px] rounded-md gap-2 items-center border border-solid border-gray-300 before:absolute before:w-full before:bottom-0 before:left-0 before:border before:border-b-2 before:border-solid before:border-b-green before:rounded-bl-md before:rounded-br-md">
       <LiaSearchSolid className="text-gray-500" size={14} />
       <input
         autoFocus
-        className="outline-none border-none text-xs w-full mr-4"
+        className="outline-none text-xs w-full mr-4"
         type="text"
+        ref={inputRef}
         placeholder="Search or start a new chat"
       />
     </div>
