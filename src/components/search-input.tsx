d8b478f4bd5f60604a9a3f2a3415695495c68065
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-01-07 20:55:49 +0800

    feat: set isInputFocused to true when search input initially loads

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,16 +1,28 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { LiaSearchSolid } from "react-icons/lia";
+import { twMerge } from "tailwind-merge";
 
 const SearchInput = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  useEffect(() => {
+    setIsInputFocused(true);
+  }, []);
   return (
-    <div className="inline-flex relative px-2 w-full h-[32px] rounded-md gap-2 items-center border border-solid border-gray-300 before:absolute before:w-full before:bottom-0 before:left-0 before:border before:border-b-2 before:border-solid before:border-b-green before:rounded-bl-md before:rounded-br-md">
+    <div
+      className={twMerge(
+        "inline-flex relative px-2 w-full h-[32px] rounded-md gap-2 items-center border border-solid border-gray-300",
+        isInputFocused &&
+          "before:absolute before:w-full before:bottom-0 before:left-0 before:border before:border-b-2 before:border-solid before:border-b-green before:rounded-bl-md before:rounded-br-md"
+      )}
+    >
       <LiaSearchSolid className="text-gray-500" size={14} />
       <input
         autoFocus
         className="outline-none text-xs w-full mr-4"
         type="text"
-        ref={inputRef}
+        onFocus={() => setIsInputFocused(true)}
+        onBlur={() => setIsInputFocused(false)}
         placeholder="Search or start a new chat"
       />
     </div>
