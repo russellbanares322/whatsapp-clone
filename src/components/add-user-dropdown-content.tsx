2fca1fc9a47ea3b3be55e854e8934da9cbe475d9
russell-banares <russell.banares@abedatasolutions.com.ph>
2025-01-11 10:37:11 +0800

    feat: display dummy contacts in AddUserDropdownContent component

--- src/components/add-user-dropdown-content.tsx
+++ src/components/add-user-dropdown-content.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import SearchInput from "./search-input";
 
+const DUMMY_CONTACTS = new Array(3).fill("").map((_) => ({
+  name: "Test",
+  isContactSavedInPhone: true,
+}));
+
 const AddUserDropdownContent = () => {
   return (
     <div className="px-2 pb-1">
       <p className="font-semibold text-base pt-2 py-2">Add User</p>
       {/* Search Input */}
       <SearchInput />
+      <div className="mt-4">{JSON.stringify(DUMMY_CONTACTS)}</div>
     </div>
   );
 };
